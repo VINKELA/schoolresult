f039a0a4d03cee1f50b40737cad107b8640ffaf3
orjikalu <orjikalukelvin@gmail.com>
2019-09-28 22:33:38 +0100

    faith

--- app.py
+++ app.py
@@ -1975,5 +1975,3 @@ def password_changer():
     error="password changed successfully"
     return render_portfolio(error)
 
-port = int(os.environ.get("PORT", 5000))
-app.run(debug=True, host='0.0.0.0', port=port)
