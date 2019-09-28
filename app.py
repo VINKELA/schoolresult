0078a2cb4d5dbd6613c8f31274f929f290d0ffeb
orjikalu <orjikalukelvin@gmail.com>
2019-09-28 22:09:01 +0100

    nothing to sat

--- app.py
+++ app.py
@@ -1975,3 +1975,5 @@ def password_changer():
     error="password changed successfully"
     return render_portfolio(error)
 
+port = int(os.environ.get("PORT", 5000))
+app.run(debug=True, host='0.0.0.0', port=port)
