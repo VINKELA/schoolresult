726b2b3519d8ae3df38909c9a8c11326b04163d7
orjikalu <orjikalukelvin@gmail.com>
2019-11-11 19:11:17 +0100

    cache

--- app.py
+++ app.py
@@ -43,13 +43,6 @@ def confirm_token(token, expiration=3600):
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 
-# Ensure responses aren't cached
-@app.after_request
-def after_request(response):
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    response.headers["Expires"] = 0
-    response.headers["Pragma"] = "no-cache"
-    return response
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
@@ -861,7 +854,7 @@ def confirm_scoresheet():
 @check_confirmed
 def submitted():
     if session["submitting_scoresheet"]:
-        error = "Already submitting scoresheet please wait"
+        error = "scoresheet Already submitted"
         return render_portfolio(error)
     else:
         session["submitting_scoresheet"] = True
