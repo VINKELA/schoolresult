b08465fe8dc57c43922e5b3408c1ac31eec8f5ef
orjikalu <orjikalukelvin@gmail.com>
2019-11-11 09:52:01 +0100

    joseph

--- static/js/school_settings.js
+++ static/js/school_settings.js
@@ -3,10 +3,18 @@ $(function() {
   $('#customize_submit').bind('click', function() {
 // Stop form from submitting normally
 event.preventDefault();
+if ($('#general_password').val().length > 0){
+if($('#general_password').val().length < 8)
+{
+  $('#general_password').focus()
+  $('#password_msg').text("password must be 8 or more letters long")
+}
+}
+else{
 $('#customize_submit').attr('disabled', true)
 $('#cancel').attr('disabled', true)
 $('#customize_form').submit()
 $('#customize_submit').text('please wait')
-   
+}
   });
 });
